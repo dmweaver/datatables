d152fa6affb83b393a9ee53a35d93b51aede8e68
weaverd <david.weaver@dpird.wa.gov.au>
2022-10-22 07:45:54 +0800

    Got one set of data working

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ let table;
 
 window.loadTable = (json) => {
   const obj = JSON.parse(json);
-  // const data = obj.data;
-  const columns = [];
-  const data = [];
+  const data = obj.data;
+  const columns = [{title: "Company", data: "fieldData.CompanyName" },
+    { title: "Address", data: "fieldData.StreetAddress" },
+    { title: "City", data: "fieldData.City" },
+    { title: "State", data: "fieldData.State" },
+    { title: "Zip Code", data: "fieldData.Zip" },
+];
+//   const data = [
+// {First: "Fred", Last: "Nerks", Age: 50 },
+// {First: "Joe", Last: "Blo", Age: 40 },
+// {First: "Bill", Last: "Smith", Age: 50 },
+//   ];
   table = $("#dtable").DataTable({
     columns,
     data,
